a2e66eb4c2b70cac1bfee0f23daba1c718015900
raja2006-jpg <youremail@example.com>
2026-01-17 12:28:28 +0530

    fixed

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -51,7 +51,7 @@ function Hero() {
 
           {/* Main Headline */}
           
-          <h1    className="text-5xl md:text-7xl font-bold text-white mb-6 leading-tight mt-30 ">
+          <h1    className="text-5xl md:text-7xl font-bold text-white mb-6 leading-tight mt-8 " >
             Where Code Meets
             <br />
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400">
